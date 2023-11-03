07177294b99987b48b691ec3e4190209b9a09f2f
marcosefgomes <m4rc05.g0m35@gmail.com>
2023-11-03 00:29:16 -0400

    first commit

---
+++ controladores/livro.js
@@ -0,0 +1,13 @@
+const { getTodosLivros } = require("../servicos/livro");
+
+function getLivros(req, res) {
+  try {
+    const livros = getTodosLivros();
+    res.send(livros);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+}
+
+module.exports = { getLivros };
